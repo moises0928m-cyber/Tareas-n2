78b738729cf8634d5c869e03fd8fd088d55d3a1e
moises <moises0928m@gmail.com>
2025-11-05 19:37:29 -0400

    bugfix/import/component-updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Cards from './components/Cards'
-import Header from './components/header'
+import Header from './components/Header'
 import Modo from './components/Modo'
 export default function App() {
   return (
